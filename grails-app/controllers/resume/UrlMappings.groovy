57e9b1b1cd002d5d81a1ca6d7a6ff2f0aeb3e64d
S. Hong <shong257@uwo.ca>
2017-03-08 11:57:31 -0500

    20170308

--- grails-app/controllers/resume/UrlMappings.groovy
+++ grails-app/controllers/resume/UrlMappings.groovy
@@ -9,6 +9,10 @@ class UrlMappings {
             }
         }
 
+        "/api/search"(controller: "search", action: "search", method: "GET")
+        "/api/signup"(controller: "user", action: "signUp", method: "POST")
+
+
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
--- grails-app/init/resume/BootStrap.groovy
+++ grails-app/init/resume/BootStrap.groovy
@@ -1,10 +1,28 @@
 package resume
 
+import resume.Role
+import resume.User
+import resume.UserRole
+
+
 class BootStrap {
 
+/**    def init = { servletContext ->
+*        createSaveAccount("Alba",  "Miranda")
+*    }
+**/
     def init = { servletContext ->
-        createSaveAccount("Alba",  "Miranda")
+        Role roleUser = new Role(authority: 'ROLE_USER').save(failOnError: true)
+        def createAccount = { String firstName, String lastName ->
+            def User = new User(firstName: firstName, lastName: lastName, userName: firstName.toLowerCase(),
+                    password: 'password').save(failOnError: true)
+            UserRole.create User, roleUser
+        }
+        createAccount 'Harry', 'Potter'
+        createAccount 'James', 'Dean'
+
     }
+
 
     def destroy = {
     }
